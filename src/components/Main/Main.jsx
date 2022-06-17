ed688c0ce9e777c13444c4c721f9bd4d3a490f50
sokolov <operator_nss@mail.ru>
2022-06-17 16:57:50 +0300

    all done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,102 +1,96 @@
-import React, {useEffect, useState} from 'react';
-import Todo from "../Todo/Todo";
-import AddTodo from "../AddTodo/AddTodo";
-import './main.css';
+import React, { useEffect, useState } from 'react'
+import Todo from '../Todo/Todo'
+import AddTodo from '../AddTodo/AddTodo'
+import './main.css'
 
 const Main = () => {
-	
-	const [todos, setTodos] = useState([]);
-	
-	
+	const [todos, setTodos] = useState([])
+
 	const initialLocalStorage = () => {
-		let todoLocalStorage = JSON.parse(localStorage.getItem('todos'));
+		let todoLocalStorage = JSON.parse(localStorage.getItem('todos'))
 		if (todoLocalStorage) {
-			setTodos(todoLocalStorage);
+			setTodos(todoLocalStorage)
 		}
 	}
-	
+
 	const updateLocal = (todos) => {
 		localStorage.setItem('todos', JSON.stringify(todos))
 	}
-	
-	
+
 	useEffect(() => {
-		initialLocalStorage();
-	}, []);
-	
-	
+		initialLocalStorage()
+	}, [])
+
 	const changeTodo = (id) => {
-		const copyData = [...todos];
-		const current = copyData.find(item => id === item._id)
-		current.isComplete = !current.isComplete;
-		setTodos(copyData);
-		filterTodos(copyData);
+		const copyData = [...todos]
+		const current = copyData.find((item) => id === item._id)
+		current.isComplete = !current.isComplete
+		setTodos(copyData)
+		filterTodos(copyData)
 	}
-	
-	
+
 	const reRenderNotDeletedItems = () => {
-		const allTodo = document.querySelectorAll('.todoItem');
-		allTodo.forEach(item => item.classList.add('rerender'));
+		const allTodo = document.querySelectorAll('.todoItem')
+		allTodo.forEach((item) => item.classList.add('rerender'))
 		setTimeout(() => {
-			allTodo.forEach(item => item.classList.remove('rerender'));
-		}, 1000);
+			allTodo.forEach((item) => item.classList.remove('rerender'))
+		}, 1000)
 	}
-	
+
 	const renderNotDeletedItems = () => {
-		const allTodo = document.querySelectorAll('.todoItem');
-		allTodo.forEach(item => item.classList.add('render'));
+		const allTodo = document.querySelectorAll('.todoItem')
+		allTodo.forEach((item) => item.classList.add('render'))
 		setTimeout(() => {
-			allTodo.forEach(item => item.classList.remove('render'));
-		}, 1000);
+			allTodo.forEach((item) => item.classList.remove('render'))
+		}, 1000)
 	}
-	
-	
-	const deleteTodo = id => {
-		const copyTodos = [...todos];
-		const current = copyTodos.filter(item => item._id !== id);
-		
+
+	const deleteTodo = (id) => {
+		const copyTodos = [...todos]
+		const current = copyTodos.filter((item) => item._id !== id)
+
 		setTimeout(() => {
-			reRenderNotDeletedItems();
+			reRenderNotDeletedItems()
 		}, 0)
-		
-		// setTimeout(() => {
-			
-			
-			setTimeout(() => {
-				renderNotDeletedItems();
-			}, 600);
-			
-			setTimeout(() => {
-				setTodos(current);
-				filterTodos(current);
-			}, 500)
-		
 
-			
+		setTimeout(() => {
+			renderNotDeletedItems()
+		}, 600)
 
-		// }, 550)
-		
+		setTimeout(() => {
+			setTodos(current)
+			filterTodos(current)
+		}, 500)
 	}
-	
+
 	const filterTodos = (todos) => {
 		setTimeout(() => {
-			const copyTodos = [...todos];
-			const notCompletedTodos = copyTodos.filter(item => item.isComplete === false);
-			const completedTodos = copyTodos.filter(item => item.isComplete === true);
-			const currentTodos = [...notCompletedTodos, ...completedTodos];
-			setTodos(currentTodos);
-			updateLocal(currentTodos);
+			const copyTodos = [...todos]
+			const notCompletedTodos = copyTodos.filter(
+				(item) => item.isComplete === false
+			)
+			const completedTodos = copyTodos.filter(
+				(item) => item.isComplete === true
+			)
+			const currentTodos = [...notCompletedTodos, ...completedTodos]
+			setTodos(currentTodos)
+			updateLocal(currentTodos)
 		}, 0)
 	}
-	
-	
+
 	return (
 		<div>
-			{todos.map(todo =>
-				(<Todo key={todo._id} todo={todo} changeTodo={changeTodo} deleteTodo={deleteTodo}/>))}
-			<AddTodo todos={todos} updateLocal={updateLocal} setTodos={setTodos}/>
+			{todos.map((todo) => (
+				<Todo
+					key={todo._id}
+					todo={todo}
+					changeTodo={changeTodo}
+					deleteTodo={deleteTodo}
+				/>
+			))}
+			<AddTodo todos={todos} updateLocal={updateLocal} setTodos={setTodos} />
 		</div>
-	);
-};
+	)
+}
 
-export default Main;
+export default Main
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,45 +1,47 @@
-import React from 'react';
-import {BsCheck} from "react-icons/bs";
-import {TiDeleteOutline} from "react-icons/ti";
-import cn from 'classnames';
-import './todo.css';
+import React from 'react'
+import { BsCheck } from 'react-icons/bs'
+import { TiDeleteOutline } from 'react-icons/ti'
+import cn from 'classnames'
+import './todo.css'
 
-const Todo = ({todo, changeTodo, deleteTodo}) => {
+const Todo = ({ todo, changeTodo, deleteTodo }) => {
 	return (
-		
-		<div className='todoItem w-full bg-slate-700 justify-between rounded-2xl mb-5 flex p-3 items-center m-3 '>
-			
+		<div className="todoItem w-full bg-slate-700 justify-between rounded-2xl mb-5 flex p-3 items-center m-3 ">
 			<button
 				className={'flex  w-full justify-between items-center '}
 				onClick={() => changeTodo(todo._id)}
 			>
 				<BsCheck
 					size={24}
-					style={todo.isComplete ? {color: '#15FF00'} : {color: '#FFFFFF4A'}}
-					className='block mr-3 bg-pink-700 rounded-lg duration-500 '/>
-				
+					style={
+						todo.isComplete ? { color: '#15FF00' } : { color: '#FFFFFF4A' }
+					}
+					className="block mr-3 bg-pink-700 rounded-lg duration-500 "
+				/>
+
 				<span className={'flex flex-wrap w-full justify-between '}>
-					
-					<span className={
-						cn('text-2xl p-1 ', {'line-through': todo.isComplete,})}>
+					<span
+						className={cn('text-2xl p-1 ', { 'line-through': todo.isComplete })}
+					>
 						{todo.title}
 					</span>
-				<span className={
-					cn(' text-2xl mr-8 p-1 ', {'line-through': todo.isComplete,})}>
+					<span
+						className={cn(' text-2xl mr-8 p-1 ', {
+							'line-through': todo.isComplete
+						})}
+					>
 						{todo.date}
 					</span>
-					
 				</span>
-			
-			
 			</button>
-			
+
 			<TiDeleteOutline
 				size={34}
-				className='hover:text-pink-700 duration-500 cursor-pointer '
+				className="hover:text-pink-700 duration-500 cursor-pointer "
 				onClick={() => deleteTodo(todo._id)}
 			/>
-		</div>);
-};
+		</div>
+	)
+}
 
-export default Todo;
+export default Todo
